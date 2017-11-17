8876ca75304cea7f6058cb4e5ba46ecb6c8488bf
Volodymyr.Voyevidka <voevidka.vova.v@gmail.com>
2017-11-17 12:32:42 +0200

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-npm version patch
 npm run production
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
-git tag $PACKAGE_VERSION
 git add dist
-git commit -m "bump version $PACKAGE_VERSION"
+npm version patch
 git push --tags
 git subtree push --prefix dist origin gh-pages
