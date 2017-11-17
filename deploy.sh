80cc2e032f13195bbb058393adc16cce768883b1
Volodymyr.Voyevidka <voevidka.vova.v@gmail.com>
2017-11-17 12:39:56 +0200

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ npm version patch --no-git-tag-version
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 npm run production
 git add .
-git commit -m "bump version $PACKAGE_VERSION"
+git commit -m "v$PACKAGE_VERSION"
 git tag "v$PACKAGE_VERSION"
 git push --tags
 git subtree push --prefix dist origin gh-pages
