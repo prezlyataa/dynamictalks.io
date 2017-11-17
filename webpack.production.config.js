bbf5e8c48fba82956128824a43a80e5bb3903b30
Volodymyr.Voyevidka <voevidka.vova.v@gmail.com>
2017-11-17 15:58:42 +0200

    v2.0.13

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -69,7 +69,7 @@ const config = {
       },
       { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=images/[name].[hash].[ext]' },
       { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader?name=fonts/[name].[hash].[ext]' },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[hash].[ext]' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=/fonts/[name].[hash].[ext]' },
       { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[hash].[ext]' },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=50000&mimetype=image/svg+xml&name=images/[name].[hash].[ext]' },
     ]
