5ecb23d974a4a2eaa7ee60292cc293cba5b3cf19
Volodymyr.Voyevidka <voevidka.vova.v@gmail.com>
2017-11-17 16:15:40 +0200

    v2.0.14

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -41,7 +41,7 @@ const config = {
       beautify: false
     }),
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
-    new ExtractTextPlugin({ filename: './[name].[hash].css', disable: false, allChunks: true }),
+    new ExtractTextPlugin({ filename: './styles/[name].[hash].css', disable: false, allChunks: true }),
     new CopyWebpackPlugin([
       { from: './images/*', to: './images/[name].[hash].[ext]' },
       { from: './public', to: './'}
@@ -68,10 +68,10 @@ const config = {
         }),
       },
       { test: /\.(png|jpg|gif)$/, use: 'url-loader?limit=15000&name=images/[name].[hash].[ext]' },
-      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader?name=fonts/[name].[hash].[ext]' },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=/fonts/[name].[hash].[ext]' },
-      { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[hash].[ext]' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=50000&mimetype=image/svg+xml&name=images/[name].[hash].[ext]' },
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader?name=fonts/[name].[ext]' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]' },
+      { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=50000&mimetype=image/svg+xml&name=images/[name].[ext]' },
     ]
   },
 };
