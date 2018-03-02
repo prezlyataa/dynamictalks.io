bda96e6abf7758545885f3ab0374848d91b5748c
Yuriy <prezlyata.yuriy@gmail.com>
2018-03-02 22:48:14 +0200

    delete load more btn

--- src/components/PreviousSpeakers/PreviousSpeakers.js
+++ src/components/PreviousSpeakers/PreviousSpeakers.js
@@ -20,50 +20,11 @@ export default class PreviousSpeakers extends Component {
     config: PropTypes.object.isRequired
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      limit: 4
-    };
-    this.onLoadMore = this.onLoadMore.bind(this);
-  }
-
-  onLoadMore() {
-    this.setState({
-      limit: this.state.limit + 2
-    });
-  }
-
-
-  renderSpeakersDesktop() {
+  renderSpeakers() {
     const { config } = this.props;
 
     return config
       .previousSpeakers
-      .map(({name, position, imageSrc}, index) => (
-        <div
-          className={cx(`${CN}_list_speaker`)}
-          key={index}
-        >
-          <img
-            alt="speaker-img"
-            className={cx(`${CN}_list_speaker_img`)}
-            src={imageSrc}
-          />
-          <p className={cx(`${CN}_list_speaker_name`)}>{name}</p>
-          <p className={cx(`${CN}_list_speaker_position`)}>{position}</p>
-        </div>
-      ));
-  }
-
-
-  renderSpeakersMobile() {
-    const { config } = this.props;
-    const { limit } = this.state;
-
-    return config
-      .previousSpeakers
-      .slice(0, limit)
       .map(({name, position, imageSrc}, index) => (
         <div
           className={cx(`${CN}_list_speaker`)}
@@ -87,20 +48,7 @@ export default class PreviousSpeakers extends Component {
         <Heading title={config.titles.previous_speakers_section}/>
         <div className="desktop">
           <div className={cx(`${CN}_list`)}>
-            {this.renderSpeakersDesktop()}
-          </div>
-        </div>
-        <div className="mobile">
-          <div className={cx(`${CN}_list`)}>
-            {this.renderSpeakersMobile()}
-          </div>
-          <div className={cx(`${CN}_btn`)}>
-            <button
-              className={cx(`${CN}_btn_loadmore`)}
-              onClick={this.onLoadMore}
-            >
-              {config.loadmoreButton.text}
-            </button>
+            {this.renderSpeakers()}
           </div>
         </div>
         <RegisterBlock config={config}/>
