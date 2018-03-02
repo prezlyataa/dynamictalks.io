2e18a329ef383eb1a0927fd1743a62ca67d7a70f
Yuriy <prezlyata.yuriy@gmail.com>
2018-03-02 12:46:04 +0200

    partners component done

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -32,7 +32,7 @@ export default class Partners extends Component {
             <div className={cx(`${CN}_general_content_logo`)}>
               <img
                 alt="logo"
-                src=""
+                src="../../images/gd-logo.png"
               />
             </div>
             <div className={cx(`${CN}_general_content_desc`)}>
@@ -62,63 +62,6 @@ export default class Partners extends Component {
                   Відкриті вакансії
                 </a>
               </div>
-            </div>
-          </div>
-        </div>
-        <div className={cx(`${CN}_informational`)}>
-          <p className={cx(`${CN}_informational_title`)}>
-            Інформаційні
-          </p>
-
-          <div className={cx(`${CN}_general_content`)}>
-            <div className={cx(`${CN}_general_content_logo`)}>
-              <img
-                alt="logo"
-                src=""
-              />
-            </div>
-            <div className={cx(`${CN}_general_content_desc`)}>
-              <p className={cx(`${CN}_general_content_desc_title`)}>Sigma software</p>
-              <p className={cx(`${CN}_general_content_desc_text`)}>
-                Grid Dynamics is the engineering IT services company known for transformative, mission-critical cloud solutions for retail, finance and technology sectors.
-              </p>
-              <div className={cx(`${CN}_general_content_desc_icons`)}>
-                <a href={config.externalEndpoints.facebook}>
-                  <img
-                    alt="icon"
-                    src="../../images/facebook.png"
-                  />
-                </a>
-                <a href={config.externalEndpoints.linkedIn}>
-                  <img
-                    alt="icon"
-                    src="../../images/linkedin-1.png"
-                  />
-                </a>
-              </div>
-              <div className={cx(`${CN}_general_content_desc_btn`)}>
-                <a
-                  className={cx(`${CN}_general_content_desc_btn_regbtn`)}
-                  href={config.externalEndpoints.vacanciesUrl}
-                >
-                  Відкриті вакансії
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className={cx(`${CN}_general_content`)}>
-            <div className={cx(`${CN}_general_content_logo`)}>
-              <img
-                alt="logo"
-                src=""
-              />
-            </div>
-            <div className={cx(`${CN}_general_content_desc`)}>
-              <p className={cx(`${CN}_general_content_desc_title`)}>Lviv IT cluster</p>
-              <p className={cx(`${CN}_general_content_desc_text`)}>
-                Grid Dynamics is the engineering IT services company known for transformative, mission-critical cloud solutions for retail, finance and technology sectors.
-              </p>
             </div>
           </div>
         </div>
